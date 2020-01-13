319abf38beffc92e22837cdbabd9b880f842c9a0
jasonwynn10 <jasonwynn10@me.com>
2020-01-13 09:40:54 -0500

    Added Support for update-able Enums

--- src/jasonwynn10/EasyCommandAutofill/Main.php
+++ src/jasonwynn10/EasyCommandAutofill/Main.php
@@ -7,7 +7,9 @@ use pocketmine\event\server\DataPacketSendEvent;
 use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
 use pocketmine\network\mcpe\protocol\types\CommandData;
 use pocketmine\network\mcpe\protocol\types\CommandEnum;
+use pocketmine\network\mcpe\protocol\types\CommandEnumConstraint;
 use pocketmine\network\mcpe\protocol\types\CommandParameter;
+use pocketmine\network\mcpe\protocol\UpdateSoftEnumPacket;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 
@@ -16,6 +18,12 @@ class Main extends PluginBase implements Listener {
 	protected $manualOverrides = [];
 	/** @var string[] $debugCommands */
 	protected $debugCommands = [];
+	/** @var CommandEnum[] $hardcodedEnums */
+	protected $hardcodedEnums = [];
+	/** @var CommandEnum[] $softEnums */
+	protected $softEnums = [];
+	/** @var CommandEnumConstraint[] $enumConstraints */
+	protected $enumConstraints = [];
 
 	public function onEnable() {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -65,14 +73,115 @@ class Main extends PluginBase implements Listener {
 	}
 
 	/**
+	 * @param CommandEnum $enum
+	 *
+	 * @return self
+	 */
+	public function addHardcodedEnum(CommandEnum $enum) : self {
+		foreach($this->softEnums as $softEnum)
+			if($enum->enumName === $softEnum->enumName)
+				throw new \InvalidArgumentException("Hardcoded enum is already in soft enum list.");
+		$this->hardcodedEnums[] = $enum;
+		foreach($this->getServer()->getOnlinePlayers() as $player) {
+			$player->sendDataPacket(new AvailableCommandsPacket());
+		}
+		return $this;
+	}
+
+	/**
+	 * @return CommandEnum[]
+	 */
+	public function getHardcodedEnums() : array {
+		return $this->hardcodedEnums;
+	}
+
+	/**
+	 * @param CommandEnum $enum
+	 *
+	 * @return self
+	 */
+	public function addSoftEnum(CommandEnum $enum) : self {
+		foreach($this->hardcodedEnums as $hardcodedEnum)
+			if($enum->enumName === $hardcodedEnum->enumName)
+				throw new \InvalidArgumentException("Soft enum is already in hardcoded enum list.");
+		$this->softEnums[] = $enum;
+		$pk = new UpdateSoftEnumPacket();
+		$pk->enumName = $enum->enumName;
+		$pk->values = $enum->enumValues;
+		$pk->type = UpdateSoftEnumPacket::TYPE_ADD;
+		$this->getServer()->broadcastPacket($this->getServer()->getOnlinePlayers(), $pk);
+		return $this;
+	}
+
+	/**
+	 * @return CommandEnum[]
+	 */
+	public function getSoftEnums() : array {
+		return $this->softEnums;
+	}
+
+	/**
+	 * @param CommandEnum $enum
+	 *
+	 * @return self
+	 */
+	public function updateSoftEnum(CommandEnum $enum) : self {
+		$pk = new UpdateSoftEnumPacket();
+		$pk->enumName = $enum->enumName;
+		$pk->values = $enum->enumValues;
+		$pk->type = UpdateSoftEnumPacket::TYPE_SET;
+		$this->getServer()->broadcastPacket($this->getServer()->getOnlinePlayers(), $pk);
+		return $this;
+	}
+
+	/**
+	 * @param CommandEnum $enum
+	 *
+	 * @return self
+	 */
+	public function removeSoftEnum(CommandEnum $enum) : self {
+		$pk = new UpdateSoftEnumPacket();
+		$pk->enumName = $enum->enumName;
+		$pk->values = $enum->enumValues;
+		$pk->type = UpdateSoftEnumPacket::TYPE_REMOVE;
+		$this->getServer()->broadcastPacket($this->getServer()->getOnlinePlayers(), $pk);
+		return $this;
+	}
+
+	/**
+	 * @param CommandEnumConstraint $enumConstraint
+	 *
+	 * @return Main
+	 */
+	public function addEnumConstraint(CommandEnumConstraint $enumConstraint) : self {
+		foreach($this->hardcodedEnums as $hardcodedEnum)
+			if($enumConstraint->getEnum()->enumName === $hardcodedEnum->enumName) {
+				$this->enumConstraints[] = $enumConstraint;
+				foreach($this->getServer()->getOnlinePlayers() as $player) {
+					$player->sendDataPacket(new AvailableCommandsPacket());
+				}
+				return $this;
+			}
+		throw new \InvalidArgumentException("Soft enum is already in hardcoded enum list.");
+	}
+
+	/**
+	 * @return CommandEnumConstraint[]
+	 */
+	public function getEnumConstraints() : array {
+		return $this->enumConstraints;
+	}
+
+	/**
 	 * @param DataPacketSendEvent $event
+	 * @priority HIGHEST
 	 */
 	public function onDataPacketSend(DataPacketSendEvent $event) {
 		$pk = $event->getPacket();
 		if(!$pk instanceof AvailableCommandsPacket)
 			return;
 		$pk->commandData = [];
-		foreach($this->getServer()->getCommandMap()->getCommands() as $name => $command) {
+		foreach($this->plugin->getServer()->getCommandMap()->getCommands() as $name => $command) {
 			if(isset($pk->commandData[$command->getName()]) or $command->getName() === "help")
 				continue;
 			if(in_array($command->getName(), array_keys($this->getManualOverrides()))) {
@@ -84,11 +193,11 @@ class Main extends PluginBase implements Listener {
 				$pk->commandData[$command->getName()] = $data;
 				continue;
 			}
-			$usage = $this->getServer()->getLanguage()->translateString($command->getUsage());
+			$usage = $this->plugin->getServer()->getLanguage()->translateString($command->getUsage());
 			if(empty($usage) or $usage[0] === '%') {
 				$data = new CommandData();
 				$data->commandName = strtolower($command->getName()); //TODO: commands containing uppercase letters in the name crash 1.9.0 client
-				$data->commandDescription = $this->getServer()->getLanguage()->translateString($command->getDescription());
+				$data->commandDescription = $this->plugin->getServer()->getLanguage()->translateString($command->getDescription());
 				$data->flags = (int)in_array($command->getName(), $this->getDebugCommands());
 				$data->permission = (int)$command->testPermissionSilent($event->getPlayer());
 
@@ -115,7 +224,7 @@ class Main extends PluginBase implements Listener {
 			$data = new CommandData();
 			$data->commandName = strtolower($command->getName()); //TODO: commands containing uppercase letters in the name crash 1.9.0 client
 			$data->commandDescription = Server::getInstance()->getLanguage()->translateString($command->getDescription());
-			$data->flags = (int)in_array($command->getName(), $this->getDebugCommands()); // make command autofill blue if debug
+			$data->flags = (int)in_array($command->getName(), $this->plugin->getDebugCommands()); // make command autofill blue if debug
 			$data->permission = (int)$command->testPermissionSilent($event->getPlayer()); // hide commands players do not have permission to use
 			$enumCount = 0;
 			for($tree = 0; $tree < count($usages); ++$tree) {
@@ -126,8 +235,8 @@ class Main extends PluginBase implements Listener {
 				if($argumentCount < 0) {
 					$data = new CommandData();
 					$data->commandName = strtolower($command->getName()); //TODO: commands containing uppercase letters in the name crash 1.9.0 client
-					$data->commandDescription = $this->getServer()->getLanguage()->translateString($command->getDescription());
-					$data->flags = (int)in_array($command->getName(), $this->getDebugCommands());
+					$data->commandDescription = $this->plugin->getServer()->getLanguage()->translateString($command->getDescription());
+					$data->flags = (int)in_array($command->getName(), $this->plugin->getDebugCommands());
 					$data->permission = (int)$command->testPermissionSilent($event->getPlayer());
 
 					$aliases = $command->getAliases();
@@ -154,13 +263,14 @@ class Main extends PluginBase implements Listener {
 						$parameter->enum = $enum;
 						$parameter->flags = 1;
 						$parameter->isOptional = false;
-						$data->overloads[$tree][0] = $parameter;
+						$data->overloads[$tree][$argNumber] = $parameter;
+						$pk->hardcodedEnums[] = $enum;
 						continue;
 					}
 					$optional = $matches[1][$argNumber] === '[';
 					$paramName = strtolower($matches[2][$argNumber]);
 					if(stripos($paramName, "|") === false) {
-						if(empty($matches[3][$argNumber]) and $this->getConfig()->get("Parse-with-Parameter-Names", true)) {
+						if(empty($matches[3][$argNumber]) and $this->plugin->getConfig()->get("Parse-with-Parameter-Names", true)) {
 							if(stripos($paramName, "player") !== false or stripos($paramName, "target") !== false) {
 								$paramType = AvailableCommandsPacket::ARG_FLAG_VALID | AvailableCommandsPacket::ARG_TYPE_TARGET;
 							}elseif(stripos($paramName, "count") !== false) {
@@ -222,6 +332,7 @@ class Main extends PluginBase implements Listener {
 						$parameter->flags = 1;
 						$parameter->isOptional = $optional;
 						$data->overloads[$tree][$argNumber] = $parameter;
+						$pk->softEnums[] = $enum;
 					}
 				}
 				$aliases = $command->getAliases();
@@ -234,6 +345,30 @@ class Main extends PluginBase implements Listener {
 					$data->aliases->enumName = ucfirst($command->getName()) . "Aliases";
 					$data->aliases->enumValues = $aliases;
 				}
+				$softEnums = $this->plugin->getSoftEnums();
+				foreach($pk->softEnums as $softEnum) {
+					foreach($softEnums as $key => $enum) {
+						if($enum->enumName === $softEnum->enumName)
+							unset($softEnums[$key]);
+					}
+				}
+				$pk->softEnums = $softEnums;
+				$enums = $this->plugin->getHardcodedEnums();
+				foreach($pk->hardcodedEnums as $hardcodedEnum) {
+					foreach($enums as $key => $enum) {
+						if($enum->enumName === $hardcodedEnum->enumName)
+							unset($enums[$key]);
+					}
+				}
+				$pk->hardcodedEnums = $enums;
+				$enumConstraints = $this->plugin->getEnumConstraints();
+				foreach($pk->enumConstraints as $constrainedEnum) {
+					foreach($enumConstraints as $key => $enum) {
+						if($enum->getEnum()->enumName === $constrainedEnum->enumName)
+							unset($enumConstraints[$key]);
+					}
+				}
+				$pk->enumConstraints = $enumConstraints;
 				$pk->commandData[$command->getName()] = $data;
 			}
 		}
