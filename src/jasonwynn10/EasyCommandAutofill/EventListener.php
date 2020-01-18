0e8b905b93eed74e73b7a55ad445e8e2fe4f80bb
jasonwynn10 <jasonwynn10@me.com>
2020-01-17 20:58:30 -0500

    add player type to parser as target alias

--- src/jasonwynn10/EasyCommandAutofill/EventListener.php
+++ src/jasonwynn10/EasyCommandAutofill/EventListener.php
@@ -82,7 +82,7 @@ class EventListener implements Listener {
 			for($tree = 0; $tree < count($usages); ++$tree) {
 				$usage = $usages[$tree];
 				$commandString = explode(" ", $usage)[0];
-				preg_match_all('/(\s?[<\[]?\s*)([a-zA-Z0-9|]+)(?:\s*:?\s*)(string|int|x y z|float|mixed|target|message|text|json|command|boolean|bool)?(?:\s*[>\]]?\s?)/iu', $usage, $matches, PREG_PATTERN_ORDER, strlen($commandString));
+				preg_match_all('/(\s?[<\[]?\s*)([a-zA-Z0-9|]+)(?:\s*:?\s*)(string|int|x y z|float|mixed|target|message|text|json|command|boolean|bool|player)?(?:\s*[>\]]?\s?)/iu', $usage, $matches, PREG_PATTERN_ORDER, strlen($commandString));
 				$argumentCount = count($matches[0])-1;
 				if($argumentCount < 0) {
 					$data = new CommandData();
@@ -144,6 +144,7 @@ class EventListener implements Listener {
 								case "float":
 									$paramType = AvailableCommandsPacket::ARG_FLAG_VALID | AvailableCommandsPacket::ARG_TYPE_FLOAT;
 								break;
+								case "player":
 								case "target":
 									$paramType = AvailableCommandsPacket::ARG_FLAG_VALID | AvailableCommandsPacket::ARG_TYPE_TARGET;
 								break;
